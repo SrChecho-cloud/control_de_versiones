22ef4c6a0d1d0d91472b92e048ac5b24e79a5122
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:57:06 -0300

    Ejercicio 7

---
+++ tp_1/Ejercicio_7.py
@@ -0,0 +1,6 @@
+## lista de cinco nombres ##
+nombres = ["Juan", "María", "Pedro", "Laura", "Carlos"]
+
+## recorrer la lista de nombres utilizando un bucle for ##
+for nombre in nombres:
+    print(nombre)

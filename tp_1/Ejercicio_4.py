9633977112370d86f67fe3dd6acf39ce34d97ae8
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:56:50 -0300

    Ejercicio 4

---
+++ tp_1/Ejercicio_4.py
@@ -0,0 +1,6 @@
+## ingresar la edad de una persona y determinar si es mayor o menor de edad ##
+edad = int(input("Ingrese la edad de la persona: "))
+if edad >= 18:
+    print("La persona es mayor de edad.")
+else:
+    print("La persona es menor de edad.")

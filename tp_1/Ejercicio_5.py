30c64a08d13619e6323fa7a70ed9c5179d050362
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:56:55 -0300

    Ejercicio 5

---
+++ tp_1/Ejercicio_5.py
@@ -0,0 +1,10 @@
+## ingrese un numero ##
+numero = float(input("Ingrese un número: "))
+
+# determinar si el numero es positivo, negativo o cero ##
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")

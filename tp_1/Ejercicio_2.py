beade97ce15013a8af4589ed9bb5203e718e8856
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:55:46 -0300

    Ejercicio_2

---
+++ tp_1/Ejercicio_2.py
@@ -0,0 +1,9 @@
+## ingresa un nombre edad y altura ##
+nombre = input("Ingrese el nombre: ")
+edad = int(input("Ingrese la edad: "))
+altura = float(input("Ingrese la altura: "))
+
+## valor ingresado y sus respectivos tipos de datos ##
+print("Nombre:", nombre, "Tipo de dato:", type(nombre))
+print("Edad:", edad, "Tipo de dato:", type(edad))
+print("Altura:", altura, "Tipo de dato:", type(altura))

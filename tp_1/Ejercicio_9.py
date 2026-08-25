20114e51d0f54a3925d99d2c9d71eba1901fdefd
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:57:26 -0300

    Ejercicio 9

---
+++ tp_1/Ejercicio_9.py
@@ -0,0 +1,6 @@
+## solicitar ingredar un numero ##
+numero = float(input("Ingrese un número: "))
+
+## mostrar los numeros desde 1 hasta el numero ingresado ##
+for i in range(1, int(numero) + 1):
+    print(i)

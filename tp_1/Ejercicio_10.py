df2555f6fadc114206ef8779d1abd6495d6f89f7
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:57:45 -0300

    Ejercicio 10

---
+++ tp_1/Ejercicio_10.py
@@ -0,0 +1,11 @@
+## listas ##
+Nombre = ["ana", "pedro", "laura", "juan"]
+Nota = [8, 5, 9, 4]
+Aprobado = ["aprobado", "desaprobado", "aprobado", "desaprobado"]
+
+## recorrer las listas y mostrar los datos ##
+for i in range(len(Nombre)):
+    print("Nombre:", Nombre[i])
+    print("Nota:", Nota[i])
+    print("Aprobado:", Aprobado[i])
+    print("--------------------")

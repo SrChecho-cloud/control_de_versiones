8283e5166539381b7f3e58b8ab4220efaf6cd610
SrChecho-cloud <sergio.jaureguiberry@hotmail.com>
2026-08-25 16:57:00 -0300

    Ejercicio 6

---
+++ tp_1/Ejercicio_6.py
@@ -0,0 +1,17 @@
+## lista de numeros ##
+numeros = [10, 20, 30, 40, 50]
+
+## mostrar la lista de numeros ##
+
+for numero in numeros:
+    print(numero)
+
+## mostrar el primer elemento de la lista ##
+print("Primer elemento:", numeros[0])
+
+## mostrar el ultimo elemento de la lista ##
+print("Último elemento:", numeros[-1])
+
+## mostrar la cantidad de elementos utilizando la funcion len() ##
+print("Cantidad de elementos:", len(numeros))
+
